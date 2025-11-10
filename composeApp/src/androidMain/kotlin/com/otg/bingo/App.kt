503a85ee528c454ba3efa48456f40367eca9a433
Jeff Cunningham <jcunningham77@gmail.com>
2025-11-09 21:14:11 -0500

    Compose Preview

--- composeApp/src/androidMain/kotlin/com/otg/bingo/App.kt
+++ composeApp/src/androidMain/kotlin/com/otg/bingo/App.kt
@@ -1,8 +1,11 @@
 package com.otg.bingo
 
+import android.content.res.Configuration
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,18 +16,25 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.flow.Flow
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import kotlinx.coroutines.flow.flowOf
 import onthegobingo.composeapp.generated.resources.Res
 import onthegobingo.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-@Preview
+
 fun App(themesFlow: Flow<List<String>>) {
     MaterialTheme {
         GameThemesPager(themesFlow = themesFlow)
@@ -32,18 +42,36 @@ fun App(themesFlow: Flow<List<String>>) {
 }
 
 @Composable
-fun GameThemesPager(themesFlow: Flow<List<String>>) {
-    val themes by themesFlow.collectAsState(initial = emptyList())
-    val pagerState = rememberPagerState(pageCount = { themes.size })
-
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier.fillMaxSize()
-    ) { page ->
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = themes[page])
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+fun GameThemesPager(
+    themesFlow: Flow<List<String>> = flowOf(
+        listOf(
+            "Amusements",
+            "Animals",
+            "Foods",
+            "Sports"
+        )
+    )
+) {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    ) {
+        val themes by themesFlow.collectAsState(initial = emptyList())
+        val pagerState = rememberPagerState(pageCount = { themes.size })
+        Log.i(
+            "GameThemesPager",
+            "GameThemesPager: current page = ${pagerState.currentPage}, total pages = ${pagerState.pageCount} themes = $themes"
+        )
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = themes[page])
+            }
         }
     }
+
 }
 
 @Composable
