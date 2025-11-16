1fe08e8feb4699f8a51dff70b6e087eeda4cf6d2
Jeff Cunningham <jcunningham77@gmail.com>
2025-11-16 13:41:49 -0500

    Preview support

---
+++ composeApp/src/androidMain/kotlin/com/otg/bingo/Preview.kt
@@ -0,0 +1,20 @@
+package com.otg.bingo
+
+
+import App
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.otg.bingo.model.GameTheme
+import kotlinx.coroutines.flow.flowOf
+
+@Preview(showBackground = true)
+@Composable
+fun GameThemesPagerPreview() {
+    val mockThemes = listOf(
+        GameTheme("Sports", "https://picsum.photos/400/600?random=1"),
+        GameTheme("Music", "https://picsum.photos/400/600?random=2"),
+        GameTheme("Movies", "https://picsum.photos/400/600?random=3")
+    )
+
+    App(themesFlow = flowOf(mockThemes))
+}
