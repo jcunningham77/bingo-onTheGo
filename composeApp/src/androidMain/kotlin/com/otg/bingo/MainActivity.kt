7a1546e6ccbaa71dea38b57b0893a4a6b88669e0
Jeff Cunningham <jcunningham77@gmail.com>
2025-12-20 15:34:29 -0500

    splash screen icon-appCompat theme bleeds through into Compose

--- composeApp/src/androidMain/kotlin/com/otg/bingo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/otg/bingo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.otg.bingo
 
 import App
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,16 +15,23 @@ import com.otg.bingo.repository.BingoRepositoryImpl
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
-        installSplashScreen()
-        super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
 
+        var keepSplashOnScreen = true
+        splashScreen.setKeepOnScreenCondition { keepSplashOnScreen }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            keepSplashOnScreen = false
+        }, 2000)
+        super.onCreate(savedInstanceState)
+        actionBar?.hide()
         setContent {
             App()
         }
     }
 }
 
-@Preview(showBackground=true)
+@Preview(showBackground = true)
 @Composable
 fun AppAndroidPreview() {
     App()
