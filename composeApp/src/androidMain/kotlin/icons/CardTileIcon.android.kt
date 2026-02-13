e9040bba9010eda40e7859e4a0abfde49e26b06e
Jeff Cunningham <jcunningham77@gmail.com>
2026-02-12 20:20:16 -0500

    android vectors

--- composeApp/src/androidMain/kotlin/icons/CardTileIcon.android.kt
+++ composeApp/src/androidMain/kotlin/icons/CardTileIcon.android.kt
@@ -5,10 +5,10 @@ package icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import onthegobingo.composeapp.generated.resources.Res
-import onthegobingo.composeapp.generated.resources.balloon_dart
+import onthegobingo.composeapp.generated.resources.ic_balloon_dart
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 actual fun CardIcon.toPainter(): Painter {
-    return painterResource(Res.drawable.balloon_dart)
+    return painterResource(Res.drawable.ic_balloon_dart)
 }
