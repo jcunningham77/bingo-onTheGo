386997f2b0e5017f8ed24548cae9ab78f3678ad6
Jeff Cunningham <jcunningham77@gmail.com>
2026-03-13 10:08:20 -0400

    ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,20 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.5.0")
+        android.set(true)
+        ignoreFailures.set(false)
+        filter {
+            exclude("**/generated/**")
+            exclude("**/build/**")
+            include("**/kotlin/**")
+        }
+    }
 }
